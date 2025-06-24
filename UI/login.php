8b9e17e97fcf42483f3a7190407496e9e63730c0
Rui Mendes <1231060@isep.ipp.pt>
2025-06-24 19:01:10 +0100

    Criação do formulário de registo de funcionario no admin

--- UI/login.php
+++ UI/login.php
@@ -10,7 +10,6 @@ require_once ("../bll/handle_login.php");
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="../CSS/styleGlobal.css">
   <link rel="stylesheet" href="../CSS/styleLogin.css">
 </head>
 
