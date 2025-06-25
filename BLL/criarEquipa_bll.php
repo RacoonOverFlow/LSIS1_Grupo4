b4d426584b2d6bda85fa1576002984aa9ecd2ef8
Andre Goncalves <1230794@isep.ipp.pt>
2025-06-25 23:25:53 +0100

    criarEquipa

--- BLL/criarEquipa_bll.php
+++ BLL/criarEquipa_bll.php
@@ -4,7 +4,15 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 require_once "../DAL/criarEquipa_dal.php";
 
-function criarEquipa() {
+function isThisACallback(): bool {
+  if (empty($_POST['nomeEquipa'])
+  {
+    return false;
+  }
+  return true;
+}
+
+function displayForm() {
   $dal = new criarEquipa_DAL();
   
   $colaboradores = $dal->getColaborador(1);
@@ -13,17 +21,22 @@ function criarEquipa() {
 
 
   echo '<form method="POST" action="">';
+  echo '<label>';
+  echo '<h2>Nome da Equipa</h2>';
+  echo '<input type="text" name="nomeEquipa" placeholder="Nome da Equipa" required>';
+  echo '</label><br><br>';
+
   echo '<h3>Selecionar Colaboradores</h3>';
   foreach ($colaboradores as $colaborador) {
     echo '<label>';
-    echo '<input type="checkbox" name="colaboradores[]" value="' . $colaborador['idFuncionario'] . '"> ' . htmlspecialchars($colaborador['nomeCompelto']);
+    echo '<input type="checkbox" name="colaboradores[]" value="' . $colaborador['numeroMecanografico'] . '"> ' . htmlspecialchars($colaborador['nomeCompelto']);
     echo '</label><br>';
   }
 
   echo '<h3>Selecionar RH</h3>';
   foreach ($RH as $rh) {
     echo '<label>';
-    echo '<input type="checkbox" name="rh[]" value="' . $rh['idFuncionario'] . '"> ' . htmlspecialchars($rh['nomeCompelto']);
+    echo '<input type="checkbox" name="rh[]" value="' . $rh['numeroMecanografico'] . '"> ' . htmlspecialchars($rh['nomeCompleto']);
     echo '</label><br>';
   }
 
@@ -32,7 +45,7 @@ function criarEquipa() {
   echo '<select name="coordenador">';
   echo '<option value="">Selecione um Coordenador</option>';
   foreach ($coordenadores as $coordenador) {
-    echo '<option value="' . $coordenador['nomeCompelto'] . '">' . htmlspecialchars($coordenador['nomeCompelto']) . '</option>';
+    echo '<option value="' . $coordenador['numeroMecanografico'] . '">' . htmlspecialchars($coordenador['nomeCompleto']) . '</option>';
   }
 
   echo '</select><br><br>';
@@ -40,8 +53,39 @@ function criarEquipa() {
   echo '<button type="submit">Criar Equipa</button>';
   echo '</form>';
 
-
-
-
 }
+
+function showUI(){
+  if(!isThisACallback()){
+    displayForm();
+  }
+  else{
+    try{
+
+      $dal = new criarEquipa_DAL();
+      $idEquipa = $dal->createItem($_POST["nome"]);
+
+      // Associar às coleções (ao criar o item é devolvido e id;
+      // o $_POST['colecoes'] provem do select e contem os ids das coleçoes selecionadas
+      if ($idEquipa && isset($_POST['colaboradores'])) {
+        $dal->associarColaboradorAEquipa($idItem, $_POST['numeroMecanografico']);
+      }
+
+      if ($idEquipa && isset($_POST['rh'])) {
+        $dal->associarRHAEquipa($idItem, $_POST['numeroMecanografico']);
+      }
+
+      if ($idEquipa && isset($_POST['coordenador'])) {
+        $dal->associarCoordenadorAEquipa($idItem, $_POST['numeroMecanografico']);
+      }
+
+      header("Location: Equipas.php");
+    }
+    catch(RuntimeException $e){
+      echo "<div>".$e->getMessage()."</div>";
+    }
+  }
+}
+
+
 ?>
--- DAL/criarEquipa_dal.php
+++ DAL/criarEquipa_dal.php
@@ -10,14 +10,14 @@ class criarEquipa_DAL {
   }
 
   function getColaborador($idCargo) {
-    $query = "SELECT dp.nomeCompelto FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN cargo c ON f.idCargo = c.idCargo WHERE c.idCargo = ?";
+    $query = "SELECT dp.nomeCompleto, f.numeroMecanografico FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN dadoslogin dl on f.numeroMecanografico = dl.numeroMecanografico INNER JOIN cargo c ON dl.idCargo = c.idCargo WHERE c.idCargo = ?";
     $stmt=$this->conn->prepare($query);
     $stmt->bind_param("s", $idCargo);
     $stmt->execute();
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
   }
   function getRH($idCargo) {
-    $query = "SELECT dp.nomeCompelto FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN cargo c ON f.idCargo = c.idCargo WHERE c.idCargo = ?";
+    $query = "SELECT dp.nomeCompleto, f.numeroMecanografico FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN dadoslogin dl on f.numeroMecanografico = dl.numeroMecanografico INNER JOIN cargo c ON dl.idCargo = c.idCargo WHERE c.idCargo = ?";
     $stmt=$this->conn->prepare($query);
     $stmt->bind_param("i", $idCargo);
     $stmt->execute();
@@ -25,11 +25,43 @@ class criarEquipa_DAL {
   }
 
   function getCoordenador($idCargo) {
-    $query = "SELECT dp.nomeCompelto FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN cargo c ON f.idCargo = c.idCargo WHERE c.idCargo = ?";
+    $query = "SELECT dp.nomeCompleto, f.numeroMecanografico FROM dadospessoais dp INNER JOIN funcionario f ON dp.idDadosPessoais = f.idDadosPessoais INNER JOIN dadoslogin dl on f.numeroMecanografico = dl.numeroMecanografico INNER JOIN cargo c ON dl.idCargo = c.idCargo WHERE c.idCargo = ?";
     $stmt=$this->conn->prepare($query);
     $stmt->bind_param("i", $idCargo);
     $stmt->execute();
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
   }
+
+  function criarEquipa($nomeEquipa, $colaboradores, $coordenador, $rh) {
+    $query = "INSERT INTO equipa (nomeEquipa, coordenador) VALUES (?, ?)";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bind_param("si", $nomeEquipa, $coordenador);
+    if (!$stmt->execute()) {
+      throw new Exception("Erro ao criar equipa: " . $stmt->error);
+    }
+    
+    $equipaId = $this->conn->insert_id;
+
+    foreach ($colaboradores as $colaborador) {
+      $query = "INSERT INTO equipa_colaboradores (equipaId, colaboradorId) VALUES (?, ?)";
+      $stmt = $this->conn->prepare($query);
+      $stmt->bind_param("ii", $equipaId, $colaborador);
+      if (!$stmt->execute()) {
+        throw new Exception("Erro ao adicionar colaborador: " . $stmt->error);
+      }
+    }
+
+    foreach ($rh as $rhId) {
+      $query = "INSERT INTO equipa_rh (equipaId, rhId) VALUES (?, ?)";
+      $stmt = $this->conn->prepare($query);
+      $stmt->bind_param("ii", $equipaId, $rhId);
+      if (!$stmt->execute()) {
+        throw new Exception("Erro ao adicionar RH: " . $stmt->error);
+      }
+    }
+
+    return true;
+  }
+
 }  
 ?>
