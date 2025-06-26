d88b76bf5daaa347c3b5fedc29b24bfc6cdeaba5
Clara Carvalho <1220893@isep.ipp.pt>
2025-06-26 18:04:03 +0100

    3 graficos funcionais

---
+++ BLL/dashboard_bll.php
@@ -0,0 +1,15 @@
+<?php
+require_once "../DAL/dashboard_dal.php";
+
+header('Content-Type: application/json');
+
+$dal = new dashboard_dal();
+$dataGenero = $dal->getGeneroDistribution();
+$dataCargo = $dal->getCargoDistribution();
+$dataNacionalidade = $dal->getNacionalidadeDistribution();
+
+echo json_encode([
+    'genero' => $dataGenero,
+    'cargo' => $dataCargo,
+    'nacionalidade' => $dataNacionalidade
+]);
---
+++ UI/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="../CSS/styleDashboard.css" />
+  <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+  <script src="../jvscript/generoChart.js" defer></script>
+</head>
+<body>
+
+<div id="generoChart" style="width: 600px; height: 400px; margin: 20px auto;"></div>
+
+<div id="circulo" style="width: 600px; height: 400px; margin: 20px auto;"></div>
+
+<div id="cargoChart" style="width: 600px; height: 400px; margin: 20px auto;"></div>
+
+</body>
+</html>
