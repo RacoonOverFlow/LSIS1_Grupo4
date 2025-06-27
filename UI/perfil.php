f5616c97893db8f60725ba2d6744dee86b41d2ec
Clara Carvalho <1220893@isep.ipp.pt>
2025-06-27 12:34:48 +0100

    alteraçãos no styleGlobal e stylePerfil

--- UI/perfil.php
+++ UI/perfil.php
@@ -13,7 +13,7 @@ include "Permissoes.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Perfil</title>
-  <link rel="stylesheet" href="../CSS/styleGlobal.css">
+  <link rel="stylesheet" href="../CSS/stylePerfil.css">
 </head>
 
 <body>
