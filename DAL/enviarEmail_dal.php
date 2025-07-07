5c72572b451ee3a0d38a8edd0f02880689a051b4
Rui Mendes <1231060@isep.ipp.pt>
2025-07-07 16:11:32 +0100

    Editar email que envia alertas no administrador
    Editar email que envia alertas no administrador

---
+++ DAL/enviarEmail_dal.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "connection.php";
+
+class enviarEmail_dal{
+    private $conn;
+
+    function __construct() {
+        $dal= new connection();
+        $this->conn = $dal->getConn();
+    }
+
+    function getCredenciaisEnvioAlertas() {
+        $query = "SELECT email, password FROM emailenvioalertas";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_assoc();
+        return $result;
+    }
+    function updateCredenciaisEnvioAlertas($email, $password){
+        $query ="UPDATE emailenvioalertas SET email=?, password=?";
+        $stmt= $this->conn->prepare($query);
+        $stmt->bind_param("ss", $email, $password);
+        return $stmt->execute();
+    }
+}
