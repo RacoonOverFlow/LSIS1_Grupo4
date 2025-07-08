af09e36d2190bd4e3ba771a20afab6717a233264
José Miguel <1230650@isep.ipp.pt>
2025-07-08 15:56:35 +0100

    css changes

--- UI/alertas.php
+++ UI/alertas.php
@@ -1,5 +1,6 @@
 <?php 
 require_once "../BLL/alertas_bll.php";
+require_once "../BLL/Permissoes.php";
 session_start();
 ?>
 
@@ -9,9 +10,11 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../CSS/stylevisualizarFuncionario.css">
-    <link rel="stylesheet" href="../CSS/styleEquipas.css">
+    <link rel="stylesheet" href="../CSS/styleGlobal.css">
+    <link rel="stylesheet" href="styleAlertas.css">
     <script src="../jvscript/alertas.js"></script>
 </head>
 <body>
@@ -20,6 +23,7 @@ session_start();
             <?php
             if(isset($_SESSION["idCargo"]) && $_SESSION['idCargo'] > 3){
                 mostrarFuncionarios();
+                mostrarSidebar();
             }
             ?>
         </main>
