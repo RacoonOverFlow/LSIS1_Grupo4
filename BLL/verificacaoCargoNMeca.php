e3859e087053cd962ed5d05bbcaceac3b4575f04
Rui Mendes <1231060@isep.ipp.pt>
2025-07-09 22:49:38 +0100

    bug fix
    bug fix

--- BLL/verificacaoCargoNMeca.php
+++ BLL/verificacaoCargoNMeca.php
@@ -1,9 +1,9 @@
 <?php
 function verificarSESSIONDados() {
-   if (!isset($_SESSION['nMeca']) || !isset($_SESSION['idCargo'])) {
-    error_log("Redirecionando para login: Sessão incompleta");
-    header("Location: login.php");
-    exit;
-}
+    if (!isset($_SESSION['nMeca']) || !isset($_SESSION['idCargo'])) {
+        error_log("Redirecionando para login: Sessão incompleta");
+        header("Location: login.php");
+        exit;
+    }
 }
 ?>
--- UI/registoFuncionario.php
+++ UI/registoFuncionario.php
@@ -1,5 +1,9 @@
 <?php
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once "../BLL/registoFuncionario_bll.php";
 require_once __DIR__ . '/../BLL/verificacaoCargoNMeca.php';
 
